4f990cf7437769bf0c4ea036c9c3a4d20e164f21
Mauricio <mauricio_cy@hotmail.com>
2025-04-03 16:48:09 -0300

    errors handling and repository pattern

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -2,6 +2,7 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 import { RegisterService } from "../services/register";
 import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
+import { UserAlreadyExistsError } from "../services/errors/user-already-exists-error";
 
 export async function registerController(
   req: FastifyRequest,
@@ -22,7 +23,10 @@ export async function registerController(
 
     await registerService.execute({ name, email, password });
   } catch (error) {
-    return res.status(409).send();
+    if (error instanceof UserAlreadyExistsError)
+      return res.status(409).send({ message: error.message });
+
+    throw error;
   }
 
   return res.status(201).send();
--- src/http/services/register.ts
+++ src/http/services/register.ts
@@ -1,5 +1,6 @@
 import { UsersRepository } from "@/repositories/users-repository";
 import { hash } from "bcryptjs";
+import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
 
 interface RegisterServiceReq {
   name: string;
@@ -19,7 +20,7 @@ export class RegisterService {
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email);
 
-    if (userWithSameEmail) throw new Error("Email already exists");
+    if (userWithSameEmail) throw new UserAlreadyExistsError();
 
     await this.usersRepository.create({ name, email, passwordHash });
   }
